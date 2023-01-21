1c488ad63e4c4432490e658dfa903a3bc0f6bbbf
Dihan141 <rhidwanrashid@iut-dhaka.edu>
2023-01-21 15:27:31 +0600

    little correction

--- Lab 1/200042149_L01_T01_1A.cpp
+++ Lab 1/200042149_L01_T01_1A.cpp
@@ -13,26 +13,28 @@ int main()
             cin>>adjMat[i][j];
     }
 
-    int celeb = 0;
+    int celeb = -1;
+    int index = -1;
     for(int i=0; i<n; i++)
     {
         int cnt = 0;
-        for(int j=0; j<n; j++)
+        int j = 0;
+        for(j=0; j<n; j++)
         {
             if(adjMat[j][i] == 1)
             {
                 cnt++;
             }
         }
-
         if(cnt == n)
         {
             celeb = i;
+            index = j;
             break;
         }
     }
 
-    if(celeb == 0)
+    if(celeb == -1)
     {
         cout<<"There is no celebrity."<<endl;
     }
@@ -43,7 +45,7 @@ int main()
         {
             if(celeb != i)
             {
-                if(adjMat[celeb][i] == 1)
+                if(adjMat[index][i] == 1)
                 {
                     flg = 1;
                     break;
